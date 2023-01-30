65347d1dc5d512b3108a5f65027434856cba47a7
bksoltan <69068724+bksoltan@users.noreply.github.com>
2023-01-30 18:21:46 +0800

    Add files via upload

---
+++ nakamonsta/start.js
@@ -0,0 +1,7 @@
+const Web3 = require('web3');
+const { node } = require('webpack');
+web3 = new Web3(new Web3.providers.HttpProvider("Http://127.0.0.1:8545"));
+
+node
+Web3 = require('web3')
+web3 = new Web3(new Web3.providers.HttpProvider("Http://127.0.0.1:8545"));
